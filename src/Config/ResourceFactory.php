7e4e7678cb7665c70505190e7cdd13d538b85b07
Fabio Pellati <fabio.pellati@gmail.com>
2017-10-07 17:35:04 +0200

    modifica cartella autoload

---
+++ src/Config/ResourceFactory.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ApigilityTools\Config;
+
+use Zend\Stdlib\ArrayUtils;
+use ZF\Configuration\ConfigResource;
+
+class ResourceFactory
+    extends \ZF\Configuration\ResourceFactory
+{
+
+    public function factory($moduleName)
+    {
+
+        $moduleName = $this->normalizeModuleName($moduleName);
+        if (isset($this->resources[$moduleName])) {
+            return $this->resources[$moduleName];
+        }
+        $moduleConfigPath = $this->modules->getModuleConfigPath($moduleName);
+        $config = include $moduleConfigPath;
+        $this->mergeAutoloadConfig($config, $moduleConfigPath);
+        $this->resources[$moduleName] = new ConfigResource($config, $moduleConfigPath, $this->writer);
+
+        return $this->resources[$moduleName];
+
+    }
+
+    protected function mergeAutoloadConfig(&$config, $moduleConfigPath)
+    {
+        if (is_dir(dirname($moduleConfigPath) . '/autoload')) {
+            $Directory = new \RecursiveDirectoryIterator(dirname($moduleConfigPath) . '/autoload');
+            $Iterator = new \RecursiveIteratorIterator($Directory);
+            $Regex = new \RegexIterator($Iterator, '#^.+\/autoload/.+(rest|rpc|api).+config\.php$#i',
+                                        \RecursiveRegexIterator::GET_MATCH);
+            $autoloadConfig = [];
+            foreach ($Regex as $file) {
+                $fileConfig = include $file[0];
+                $autoloadConfig = ArrayUtils::merge($autoloadConfig, $fileConfig);
+            }
+            $config = ArrayUtils::merge($config, $autoloadConfig);
+
+        }
+
+    }
+}
