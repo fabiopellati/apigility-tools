26f5c3f27751f81e724bb864f95e43100d276109
Fabio Pellati <fabio.pellati@gmail.com>
2017-10-14 14:00:55 +0200

    introduzione domainException

---
+++ src/Exception/DomainException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ * apigility-tools (https://github.com/fabiopellati/apigility-tools)
+ *
+ * @link      https://github.com/fabiopellati/apigility-tools for the canonical source repository
+ * @copyright Copyright (c) 2017 Fabio Pellati (https://github.com/fabiopellati)
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ *
+ */
+
+namespace ApigilityTools\Exception;
+class DomainException
+    extends \Exception
+{
+
+}
