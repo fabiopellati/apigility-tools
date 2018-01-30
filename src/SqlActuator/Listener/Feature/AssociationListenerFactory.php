befd303aeb3d67b480e006f7481f5eff99b9c71f
Fabio Pellati <fabio.pellati@gmail.com>
2018-01-30 16:42:41 +0100

    clean comments

--- src/SqlActuator/Listener/Feature/AssociationListenerFactory.php
+++ src/SqlActuator/Listener/Feature/AssociationListenerFactory.php
@@ -12,9 +12,6 @@
 namespace ApigilityTools\SqlActuator\Listener\Feature;
 
 use Interop\Container\ContainerInterface;
-use Interop\Container\Exception\ContainerException;
-use Zend\ServiceManager\Exception\ServiceNotCreatedException;
-use Zend\ServiceManager\Exception\ServiceNotFoundException;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class AssociationListenerFactory
@@ -28,10 +25,8 @@ class AssociationListenerFactory
      * @param  null|array         $options
      *
      * @return object
-     * @throws ServiceNotFoundException if unable to resolve the service.
-     * @throws ServiceNotCreatedException if an exception is raised when
-     *     creating a service.
-     * @throws ContainerException if any other error occurs
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
--- src/SqlActuator/Listener/Feature/SoftDeleteListenerFactory.php
+++ src/SqlActuator/Listener/Feature/SoftDeleteListenerFactory.php
@@ -12,10 +12,7 @@
 namespace ApigilityTools\SqlActuator\Listener\Feature;
 
 use Interop\Container\ContainerInterface;
-use Interop\Container\Exception\ContainerException;
 use Zend\Cache\StorageFactory;
-use Zend\ServiceManager\Exception\ServiceNotCreatedException;
-use Zend\ServiceManager\Exception\ServiceNotFoundException;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 class SoftDeleteListenerFactory
@@ -29,10 +26,8 @@ class SoftDeleteListenerFactory
      * @param  null|array         $options
      *
      * @return object
-     * @throws ServiceNotFoundException if unable to resolve the service.
-     * @throws ServiceNotCreatedException if an exception is raised when
-     *     creating a service.
-     * @throws ContainerException if any other error occurs
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
