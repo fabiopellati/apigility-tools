b8b34107c26e87c153e8cd3812243485f34e742f
Fabio Pellati <fabio.pellati@gmail.com>
2018-02-15 11:15:31 +0100

    add examples

---
+++ dist/config/rest-[service-with-has.many].config.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ *
+ * apigility-tools (https://github.com/fabiopellati/apigility-tools)
+ *
+ * @link      https://github.com/fabiopellati/apigility-tools for the canonical source repository
+ * @copyright Copyright (c) 2017 Fabio Pellati (https://github.com/fabiopellati)
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ *
+ */
+
+namespace HrmsApi\V1\Rest\ServiceHasManyName;
+return [
+    'service_manager' => [
+        'factories' => [
+            __NAMESPACE__ .
+            '\\ServiceHasManyNameResource'               => \ApigilityTools\Rest\Resource\ResourceListenerFactory::class,
+            __NAMESPACE__ . '\\Mapper'                   => \ApigilityTools\Mapper\MapperFactory::class,
+            __NAMESPACE__ . '\\ServiceHasManyNameEntity' => \ApigilityTools\Rest\Entity\EventAwareEntityFactory::class,
+        ],
+    ],
+    'apigility-tools' => [
+        'actuator-mapper' => [
+            __NAMESPACE__ . '\\ServiceHasManyNameResource' => [
+                'mapper_class' => __NAMESPACE__ . '\\Mapper',
+            ],
+            __NAMESPACE__ . '\\Mapper'                     => [
+                'namespace'                          => __NAMESPACE__,
+                'db_adapter'                         => 'Gnc\\Db\\Adapter\\Locale',
+                'db_schema'                          => 'schema',
+                'db_table'                           => 'associated_table',
+                'route_association_identifier_name'  => 'has_many_id',
+                'entity_association_identifier_name' => 'id',
+                'association_joins'                  => [
+                    [
+                        'entity_association_identifier_name' => 'service_id_in_has_table',
+                        'route_association_identifier_name'  => 'service_id_in_route',
+                        'db_schema'                          => 'has_schema',
+                        'db_table'                           => 'has_table',
+                        'on'                                 => [
+                            ['id', 'badge_id'],/*[field in associated table, field in has table]*/
+                        ],
+                        'columns'                            => ['attivo_da', 'attivo_a',],/*fields from associated*/
+                    ],
+                ],
+                'mapper-listeners'                   => [
+                    'ApigilityTools\\SqlActuator\\Listener\\Query\\WhereIdListener',
+                    'ApigilityTools\\SqlActuator\\Listener\\Feature\\AssociationManyListener',
+                ],
+            ],
+            __NAMESPACE__ . '\\ServiceHasManyNameEntity'   => [
+                'entity-listeners' => [
+                ],
+            ],
+        ],
+    ],
+];
