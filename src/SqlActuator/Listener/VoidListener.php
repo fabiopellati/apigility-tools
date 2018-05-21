7ea65d16072d759af9c9c6f939cc72fe58615b49
Fabio Pellati <fabio.pellati@gmail.com>
2018-05-21 16:38:01 +0200

    aggiunta void listener

---
+++ src/SqlActuator/Listener/VoidListener.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * apigility-tools (https://github.com/fabiopellati/apigility-tools)
+ *
+ * @link      https://github.com/fabiopellati/apigility-tools for the canonical source repository
+ * @copyright Copyright (c) 2017 Fabio Pellati (https://github.com/fabiopellati)
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ *
+ */
+
+namespace ApigilityTools\SqlActuator\Listener;
+
+use ApigilityTools\Traits\ReplaceEventAwareTrait;
+use MessageExchangeEventManager\EventManagerAwareTrait;
+use MessageExchangeEventManager\EventRunAwareTrait;
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\EventManagerAwareInterface;
+use Zend\EventManager\EventManagerInterface;
+
+class VoidListener
+    extends AbstractListenerAggregate
+    implements EventManagerAwareInterface
+{
+    use EventManagerAwareTrait;
+    use EventRunAwareTrait;
+    use ReplaceEventAwareTrait;
+
+    /**
+     * @param \Zend\EventManager\EventManagerInterface $events
+     * @param int                                      $priority
+     *
+     */
+    public function attach(EventManagerInterface $events, $priority = 100)
+    {
+
+
+    }
+
+}
